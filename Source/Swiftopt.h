d9f9a4397624ecdf2078cc4632353b7e8d1e44fa
Adam Sharp <adam@outware.com.au>
2015-02-17 07:02:58 +1100

    Initial commit

---
+++ Source/Swiftopt.h
@@ -0,0 +1,19 @@
+//
+//  Swiftopt.h
+//  Swiftopt
+//
+//  Created by Adam Sharp on 17/02/2015.
+//  Copyright (c) 2015 Adam Sharp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swiftopt.
+FOUNDATION_EXPORT double SwiftoptVersionNumber;
+
+//! Project version string for Swiftopt.
+FOUNDATION_EXPORT const unsigned char SwiftoptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftopt/PublicHeader.h>
+
+
---
+++ Tests/SwiftoptTests.swift
@@ -0,0 +1,10 @@
+//  Copyright (c) 2015 Adam Sharp. All rights reserved.
+
+import XCTest
+import Swiftopt
+
+class SwiftoptTests: XCTestCase {
+	func test() {
+		XCTAssert(true)
+	}
+}
