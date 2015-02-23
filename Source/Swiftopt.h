d42689cdc5bdc569b52c282b44afd342038048ff
Adam Sharp <adam@outware.com.au>
2015-02-23 22:46:59 +1100

    Import Foundation in umbrella header and remove stale comments

--- Source/Swiftopt.h
+++ Source/Swiftopt.h
@@ -6,14 +6,10 @@
 //  Copyright (c) 2015 Adam Sharp. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Swiftopt.
 FOUNDATION_EXPORT double SwiftoptVersionNumber;
 
 //! Project version string for Swiftopt.
 FOUNDATION_EXPORT const unsigned char SwiftoptVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Swiftopt/PublicHeader.h>
-
-
